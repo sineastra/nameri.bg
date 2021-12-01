978915b71d44602dee46296ad7e5e78cd275cb8e
Sineastra <lord.of.light.0002@gmail.com>
2021-12-01 17:27:58 +0200

    changed rest api name

--- nameri.bg/REST_API/config/routesConfig.js
+++ nameri.bg/REST_API/config/routesConfig.js
@@ -17,24 +17,7 @@ const routesConfig = app => {
     app.use("/api/listings", listingsController)
     app.use("/api/towns", townsController)
     app.use("/", async (req, res) => {
-        const data = {
-            heading: "Listing asdasdasdasd",
-            prices: [4, 5, 6],
-            images: [
-                "../../../assets/images/service1.png",
-                "../../../assets/images/service2.png",
-                "../../../assets/images/service3.png",
-            ],
-            mainImg: "../../../assets/images/service2.png",
-            tags: ["new"],
-            premiumType: true,
-            reviews: ["61a113fdb4ad6014e993a2c6", "61a113fdb4ad6014e993a2c7"],
-            details: "This is the second listing",
-            city: "Sofia",
-            user: "61a6a0afed501169b4aaf66e",
-            rating: "5",
-        }
-        await ListingModel.collection.insertOne(data)
+        res.send("server working. make requests at /api")
     })
 }
 
