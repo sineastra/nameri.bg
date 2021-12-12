10b4d0abab71fda1f9ee5950aa6582d6ab7878a4
Sineastra <lord.of.light.0002@gmail.com>
2021-12-12 15:34:16 +0200

    testing theproblem with the vercel deploying cookies

--- nameri.bg/REST_API/controllers/userController.js
+++ nameri.bg/REST_API/controllers/userController.js
@@ -23,7 +23,7 @@ const signIn = async (req, res) => {
             const cookie = res.cookie(process.env.COOKIE_NAME, token, {
                 sameSite: "none",
                 secure: true,
-                httpOnly: false,
+                httpOnly: true,
             })
 
             res.json({ ok: true, status: 200, statusCode: "OK", token })
