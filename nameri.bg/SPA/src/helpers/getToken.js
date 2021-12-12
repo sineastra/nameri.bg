33c35cb63382cbf959421fc7286ff841ea50f006
Sineastra <lord.of.light.0002@gmail.com>
2021-12-12 14:32:43 +0200

    testing theproblem with the vercel deploying cookies

--- nameri.bg/SPA/src/helpers/getToken.js
+++ nameri.bg/SPA/src/helpers/getToken.js
@@ -3,6 +3,9 @@ import Cookies from "js-cookie"
 // Change this if storing the token on some storage, or anywhere else than a cookie.
 
 const getToken = () => {
+	console.log(Cookies)
+	console.log(process.env.REACT_APP_JWT_COOKIE_NAME)
+
 	return Cookies.get(process.env.REACT_APP_JWT_COOKIE_NAME)
 }
 
