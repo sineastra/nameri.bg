28a5fd93f4ff374c065c94ad9007600462db243d
Sineastra <lord.of.light.0002@gmail.com>
2021-12-13 12:43:42 +0200

    added suspense and a spinner

--- nameri.bg/SPA/src/App.jsx
+++ nameri.bg/SPA/src/App.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter } from 'react-router-dom'
 import AppWrapper from "./Components/Components/AppWrapper/AppWrapper.jsx"
+import { Suspense } from "react"
+import Spinner from "./Components/Components/Spinner/Spinner.jsx"
 
 
 function App () {
 	return (
-		<BrowserRouter>
-			<AppWrapper/>
-		</BrowserRouter>
+		<Suspense fallback={ <Spinner/> }>
+			<BrowserRouter>
+				<AppWrapper/>
+			</BrowserRouter>
+		</Suspense>
 	)
 }
 
--- nameri.bg/SPA/src/Components/Components/Spinner/Spinner.jsx
+++ nameri.bg/SPA/src/Components/Components/Spinner/Spinner.jsx
@@ -4,7 +4,11 @@ import styles from "./Spinner.module.css"
 const Spinner = () => {
 
 	return (
-		<div className={ styles.loader }>Loading...</div>
+		<div className={ styles.class3 }>
+			<div className={ styles.class2 }>
+				<div></div>
+			</div>
+		</div>
 	)
 }
 
