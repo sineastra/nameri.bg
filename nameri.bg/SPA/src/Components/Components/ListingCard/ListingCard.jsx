a740fe32c01cf24a4221da5ee0ad59f5db914e23
Sineastra <lord.of.light.0002@gmail.com>
2021-12-14 14:55:27 +0200

    added image loader to the listing card

--- nameri.bg/SPA/src/Components/Components/ListingCard/ListingCard.jsx
+++ nameri.bg/SPA/src/Components/Components/ListingCard/ListingCard.jsx
@@ -1,28 +1,41 @@
 import styles from "./ListingCard.module.css"
 import { Link } from "react-router-dom"
+import ImageLoadingPlaceholder from "../ImageLoadingPlaceholder/ImageLoadingPlaceholder.jsx"
+import { useState } from "react"
 
 
 const ListingCard = ({ className, listing, user }) => {
-	const profileImg = listing.user.profileImg === "" ? "profile.svg" : listing.user.profileImg
+	const [imgLoaded, setImgLoaded] = useState(false)
+	const profileImg = listing.user.profileImg === "" ? "/profile.svg" : listing.user.profileImg
 
 	return (
-
-		<div className={ `${ styles.outerWrapper } ${ className }` }>
-			<Link to={ `/details/${ listing._id }` } className={ styles.imageWrapper }>
-				<img src={ listing.mainImg } alt="Service Main Image" className={ styles.serviceImg }/>
-				<div className={ `${ styles.comfortaa } ${ styles.townDiv }` }>
-					{ listing.town.name }
-				</div>
-			</Link>
-			<Link to={ `/details/${ listing._id }` } className={ styles.listingHeading }>{ listing.heading }</Link>
-			<div className={ styles.profileInfoWrapper }>
-				<Link to={ `/profile/${ user._id }` } className={ styles.profileImageWrapper }>
-					<img src={ profileImg || '/profile.svg' } alt="" className={ styles.profilePic }/>
-					<div className={ `${ styles.comfortaa } ${ styles.nameAndSurname }` }>{ user.nameAndSurname }</div>
+		<>
+			<div className={ `${ styles.outerWrapper } ${ className } ${ imgLoaded ? styles.show : styles.hide }` }>
+				<Link to={ `/details/${ listing._id }` } className={ styles.imageWrapper }>
+					<img
+						src={ listing.mainImg }
+						alt="Service Main Image"
+						className={ styles.serviceImg }
+						onLoad={ () => setImgLoaded(true) }
+						onError={ () => setImgLoaded(false) }
+					/>
+					<div className={ `${ styles.comfortaa } ${ styles.townDiv }` }>
+						{ listing.town.name }
+					</div>
 				</Link>
-				<div className={ styles.priceE }>{ listing.prices[0] } лв.</div>
+				<Link to={ `/details/${ listing._id }` } className={ styles.listingHeading }>{ listing.heading }</Link>
+				<div className={ styles.profileInfoWrapper }>
+					<Link to={ `/profile/${ user._id }` } className={ styles.profileImageWrapper }>
+						<img src={ profileImg } alt="" className={ styles.profilePic }/>
+						<div
+							className={ `${ styles.comfortaa } ${ styles.nameAndSurname }` }>{ user.nameAndSurname }</div>
+					</Link>
+					<div className={ styles.priceE }>{ listing.prices[0] } лв.</div>
+				</div>
 			</div>
-		</div>
+			<ImageLoadingPlaceholder
+				outerClassName={ imgLoaded ? styles.hide : `${ styles.show } ${ styles.imgLoader } ${ styles.outerWrapper } ${ className }` }/>
+		</>
 	)
 }
 
