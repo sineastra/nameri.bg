79505fdc432360fb891d25f44efb4d8ff33d7af7
Sineastra <lord.of.light.0002@gmail.com>
2021-12-19 22:04:59 +0200

    added the style for the error page

--- nameri.bg/SPA/src/Components/Pages/ErrorPage/ErrorPage.jsx
+++ nameri.bg/SPA/src/Components/Pages/ErrorPage/ErrorPage.jsx
@@ -1,15 +1,21 @@
-import { useLocation } from "react-router-dom"
+import styles from "./ErrorPage.module.css"
+import { Link } from "react-router-dom"
 
 
 const ErrorPage = (props) => {
-	const location = useLocation()
-	const serverNotResponding = location.state.statusCode === undefined && location.state.status === undefined
-
 	return (
-		serverNotResponding
-			? <div>Something went wrong. Please wait a few minutes and try again!</div>
-			: <div>{ location.state.statusCode } - { location.state.status }</div>
+		<div className={ styles.errorWrapper }>
+			<div className={ styles.errorImgDiv }/>
+			<div className={ styles.svgWrapper }>
+				<div className={ styles.svgInnerWrapper }>
+					<div className={ styles.statusDiv }>404</div>
 
+					<h3 className={ styles.header }>You're in the wrong place</h3>
+
+					<Link to="/" className={ styles.redirectBtn }>Take me back to safety, please!</Link>
+				</div>
+			</div>
+		</div>
 	)
 }
 
