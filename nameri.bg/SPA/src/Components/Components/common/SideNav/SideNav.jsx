186773439282d01774f94779b783b3bf1f88e361
Sineastra <lord.of.light.0002@gmail.com>
2022-01-08 15:00:45 +0200

    fixes

--- nameri.bg/SPA/src/Components/Components/common/SideNav/SideNav.jsx
+++ nameri.bg/SPA/src/Components/Components/common/SideNav/SideNav.jsx
@@ -5,6 +5,7 @@ import { useState } from "react"
 
 const SideNav = ({ className, toggleSideBar, user, logout }) => {
 	const [userNavVisibility, setUserNavVisibility] = useState(false)
+	const profileImg = user.profileImg && user.profileImg !== '' ? user.profileImg : "/profile.png"
 
 	const toggleUserNavVisibility = () => setUserNavVisibility(oldState => !oldState)
 	const toggleSideNavVisibility = () => {
@@ -12,41 +13,39 @@ const SideNav = ({ className, toggleSideBar, user, logout }) => {
 		toggleSideBar()
 	}
 
-	return (
-		<div className={ `${ styles.sideNav } ${ className }` }>
-			<div onClick={ toggleSideNavVisibility } className={ styles.closeBtn }>X</div>
-			{ user
-				? <div className={ styles.linksWrapper }>
-					<div className={ styles.userNavLinks }>
-						<h1 className={ styles.userNames } onClick={ toggleUserNavVisibility }>Здравей, <span
-							className={ styles.userNamesSpan }>{ user.nameAndSurname }</span></h1>
-						<div
-							className={ `${ userNavVisibility ? styles.show : styles.hidden } ${ styles.userNavLinksInner }` }>
-							<Link className={ `${ styles.userNavLinkInner } ${ styles.navLink }` }
-							      to={ `/profile/${ user._id }` }>Профил</Link>
-							<Link
-								className={ `${ styles.userNavLinkInner }  ${ styles.navLink }` }
-								to={ `/profile/edit` }>Редактирай профил</Link>
-							<Link
-								className={ `${ styles.userNavLinkInner }  ${ styles.navLink }` }
-								to={ `/add-listing` }>Добави обява</Link>
-							<div
-								className={ `${ styles.userNavLinkInner }  ${ styles.navLink } ${ styles.lastUserLink }` }
-								onClick={ logout }>Излез
-							</div>
-						</div>
+	return (<div className={ `${ styles.sideNav } ${ className }` }>
+		<div onClick={ toggleSideNavVisibility } className={ styles.closeBtn }>X</div>
+		{ user ? <div className={ styles.linksWrapper }>
+			<div className={ styles.userNavLinks }>
+				<div className={ styles.userMetaWrapper }>
+					<img src={ profileImg } alt="" className={ styles.profileImg }/>
+					<h1 className={ styles.userNames } onClick={ toggleUserNavVisibility }>Здравей, <span
+						className={ styles.userNamesSpan }>{ user.nameAndSurname }</span></h1>
+				</div>
+				<div
+					className={ `${ userNavVisibility ? styles.show : styles.hidden } ${ styles.userNavLinksInner }` }>
+					<Link className={ `${ styles.userNavLinkInner } ${ styles.navLink }` }
+					      to={ `/profile/${ user._id }` }>Профил</Link>
+					<Link
+						className={ `${ styles.userNavLinkInner }  ${ styles.navLink }` }
+						to={ `/profile/edit` }>Редактирай профил</Link>
+					<Link
+						className={ `${ styles.userNavLinkInner }  ${ styles.navLink }` }
+						to={ `/add-listing` }>Добави обява</Link>
+					<div
+						className={ `${ styles.userNavLinkInner }  ${ styles.navLink } ${ styles.lastUserLink }` }
+						onClick={ logout }>Излез
 					</div>
-					<Link className={ styles.navLink } to="/categories">Категории</Link>
-					<Link className={ styles.navLink } to="/about">За платформата</Link>
-					<Link className={ styles.navLink } to="/top-users">Топ потребители</Link>
 				</div>
-				: <div className={ styles.linksWrapper }>
-					<Link className={ styles.navLink } to="/sign-in">Влез</Link>
-					<Link className={ styles.navLink } to="/sign-up">Регистрирай се</Link>
-				</div>
-			}
-		</div>
-	)
+			</div>
+			<Link className={ styles.navLink } to="/categories">Категории</Link>
+			<Link className={ styles.navLink } to="/about">За платформата</Link>
+			<Link className={ styles.navLink } to="/top-users">Топ потребители</Link>
+		</div> : <div className={ styles.linksWrapper }>
+			<Link className={ styles.navLink } to="/sign-in">Влез</Link>
+			<Link className={ styles.navLink } to="/sign-up">Регистрирай се</Link>
+		</div> }
+	</div>)
 }
 
 export default SideNav
