4646f9361009b84c7ff131984850c0f90ced1d64
Sineastra <lord.of.light.0002@gmail.com>
2023-01-29 15:24:55 +0200

    added export for vercel

--- nameri.bg/REST_API/index.js
+++ nameri.bg/REST_API/index.js
@@ -1,13 +1,15 @@
-const express = require("express")
-const mainConfig = require("./config/mainConfig")
-const app = express()
+const express = require("express");
+const mainConfig = require("./config/mainConfig");
+const app = express();
 
 const start = async app => {
-	const port = process.env.EXPRESS_PORT || process.env.PORT || "3030"
+  const port = process.env.EXPRESS_PORT || process.env.PORT || "3030";
 
-	await mainConfig(app)
+  await mainConfig(app);
 
-	app.listen(port, () => console.log(`Listening on port ${ port }`))
-}
+  app.listen(port, () => console.log(`Listening on port ${port}`));
+};
 
-start(app)
+start(app);
+
+module.exports = app;
