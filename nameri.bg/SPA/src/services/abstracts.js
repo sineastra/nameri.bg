c0016ec326e664b5f61527ab8059fc8786f9e76e
Sineastra <lord.of.light.0002@gmail.com>
2023-04-11 19:27:21 +0300

    fixed cors

--- nameri.bg/SPA/src/services/abstracts.js
+++ nameri.bg/SPA/src/services/abstracts.js
@@ -1,55 +1,53 @@
-import processNewToken from "../helpers/processNewToken.js"
+import processNewToken from "../helpers/processNewToken.js";
 
-
-const baseUrl = `${ process.env.REACT_APP_REST_API_ADDRESS }/api`
+const baseUrl = `${process.env.REACT_APP_REST_API_ADDRESS}/api`;
 
 const abstractFetch = (url, body, method = "GET") => {
-	let predefinedBody = {
-		method,
-		headers: {
-			'Access-Control-Allow-Origin': process.env.REACT_APP_ORIGIN,
-		},
-		credentials: 'include',
-	}
-	predefinedBody = Object.assign(predefinedBody, body || {})
+  let predefinedBody = {
+    method,
+    headers: {
+      "Access-Control-Allow-Origin": process.env.REACT_APP_ORIGIN,
+    },
+    credentials: "include",
+  };
+  predefinedBody = Object.assign(predefinedBody, body || {});
 
-	return new Promise((resolve, reject) => {
-		fetch(`${ baseUrl }${ url }`, predefinedBody)
-			.then(res => res.json())
-			.then(resData => {
-				if (resData.data === undefined) {
-					reject(resData)
-				}
+  return new Promise((resolve, reject) => {
+    fetch(`${baseUrl}${url}`, predefinedBody)
+      .then(res => res.json())
+      .then(resData => {
+        if (resData.data === undefined) {
+          reject(resData);
+        }
 
-				if (resData.token) {
-					processNewToken(resData.token)
-				}
-				resolve(resData.data)
-			})
-			.catch(e => {
-				reject(e)
-			})
-
-	})
-}
+        if (resData.token) {
+          processNewToken(resData.token);
+        }
+        resolve(resData.data);
+      })
+      .catch(e => {
+        reject(e);
+      });
+  });
+};
 
 const abstractFormDataRequest = (url, formData, method) => {
-	const body = {
-		body: formData,
-	}
+  const body = {
+    body: formData,
+  };
 
-	return abstractFetch(url, body, method)
-}
+  return abstractFetch(url, body, method);
+};
 
 const abstractPostFormRequest = (url, formData, method) => {
-	const body = {
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify(formData),
-	}
+  const body = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
 
-	return abstractFetch(url, body, method)
-}
+  return abstractFetch(url, body, method);
+};
 
-export { abstractFetch, abstractFormDataRequest, abstractPostFormRequest }
+export { abstractFetch, abstractFormDataRequest, abstractPostFormRequest };
